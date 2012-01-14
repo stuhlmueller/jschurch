c154a61c74721fd0b93dbbd28b4a9f1492cb2a63
Long Ouyang <longouyang@gmail.com>
2012-01-14 00:47:08 -0800

    Cosmetic changes

--- scheme-compilers/javascript/jschurch-webservice.js
+++ scheme-compilers/javascript/jschurch-webservice.js
@@ -7,12 +7,9 @@ var evalChurchCode = function(churchCode, returnValueHandler){
   var wrappedSchemeExpr = scheme2jsTemplate.supplant(
     { churchprogram : String(schemeExpr).slice(1,-1) });
   
-  // used for caching scheme code
-  //BgL_symbolzd2indexzd2=0;
-  
   if (__codeCache[key]) {
     console.log("running from client-side cache");
-    eval(__codeCache[key])
+    eval(__codeCache[key]);
   } else {
     console.log("submitting to scheme2js server");
     var url="http://focusedattention.org/scheme2js/scheme2js.php?scheme=" +
