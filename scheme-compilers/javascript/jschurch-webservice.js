1f2e8b4827376fcdb30be18c30901a7b96096d02
Andreas Stuhlmueller <andreas@stuhlmueller.info>
2012-01-17 01:18:19 -0800

    Handle compilation and runtime errors.

--- scheme-compilers/javascript/jschurch-webservice.js
+++ scheme-compilers/javascript/jschurch-webservice.js
@@ -1,12 +1,24 @@
 var __codeCache = {};
 var evalChurchCode = function(churchCode, returnValueHandler){
-  key = hex_md5(churchCode);
-  
-  var churchInputExpr = sc_read(new sc_StringInputPort("(" + churchCode + ")"));
-  var schemeExpr = compile(churchInputExpr, null);
-  var wrappedSchemeExpr = scheme2jsTemplate.supplant(
-    { churchprogram : String(schemeExpr).slice(1,-1) });
-  
+  var key = hex_md5(churchCode);
+
+  var churchInputExpr = "";
+  var schemeExpr = "";
+  var compilationSuccess = true;
+  try {
+    churchInputExpr = sc_read(new sc_StringInputPort("(" + churchCode + ")"));
+    schemeExpr = compile(churchInputExpr, null);
+    var wrappedSchemeExpr = scheme2jsTemplate.supplant(
+      { churchprogram : String(schemeExpr).slice(1,-1) });
+  } catch (err) {
+    compilationSuccess = false;
+    returnValueHandler(err);
+  }
+
+  if (!(compilationSuccess)) {
+    return false;
+  }
+
   if (__codeCache[key]) {
     console.log("running from client-side cache");
     eval(__codeCache[key]);
@@ -21,7 +33,11 @@ var evalChurchCode = function(churchCode, returnValueHandler){
               SC_DEFAULT_OUT = new sc_GenericOutputPort(returnValueHandler);
               SC_ERROR_OUT = SC_DEFAULT_OUT;
               // Run compiled file
-              eval(compiledCode);
+              try {
+                eval(compiledCode);
+              } catch (err) {
+                returnValueHandler(err);
+              }
       });
     }
 };
