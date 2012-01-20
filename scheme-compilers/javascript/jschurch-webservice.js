612cd495b63892c8670bb60947efa74992e90524
Andreas Stuhlmueller <andreas@stuhlmueller.info>
2012-01-20 01:11:53 -0800

    Introduce small delay for cached runs. Refactoring.

--- scheme-compilers/javascript/jschurch-webservice.js
+++ scheme-compilers/javascript/jschurch-webservice.js
@@ -1,47 +1,67 @@
 var __codeCache = {};
+
+
+var safeDelayedEval = function(jsCode, errHandler, delay){
+  var safeEval = function() {
+    try {
+      eval(jsCode);
+    } catch (err) {
+      errHandler(err);
+    }};
+  setTimeout(safeEval, 100);
+};
+
+
+var scheme2jsURL = function(schemeExpr){
+  return "http://focusedattention.org/scheme2js/scheme2js.php?scheme=" +
+    encodeURIComponent(schemeExpr) +
+    "&callback=?";
+};
+
+
+var setScheme2jsPorts = function(outHandler){
+  SC_DEFAULT_OUT = new sc_GenericOutputPort(outHandler);
+  SC_ERROR_OUT = SC_DEFAULT_OUT;
+};
+
+
 var evalChurchCode = function(churchCode, returnValueHandler){
+
   var key = hex_md5(churchCode);
 
-  var churchInputExpr = "";
-  var schemeExpr = "";
-  var compilationSuccess = true;
-  try {
-    churchInputExpr = sc_read(new sc_StringInputPort("(" + churchCode + ")"));
-    schemeExpr = compile(churchInputExpr, null);
-    var wrappedSchemeExpr = scheme2jsTemplate.supplant(
-      { churchprogram : String(schemeExpr).slice(1,-1) });
-  } catch (err) {
-    compilationSuccess = false;
-    returnValueHandler(err);
-  }
-
-  if (!(compilationSuccess)) {
-    return false;
-  }
-
   if (__codeCache[key]) {
+
     console.log("running from client-side cache");
+    setScheme2jsPorts(returnValueHandler);
+    safeDelayedEval(__codeCache[key], returnValueHandler, 100);
+
+  } else {
+
+    // Compile Church to Scheme
+    var churchInputExpr = "";
+    var schemeExpr = "";
+    var compilationSuccess = true;
     try {
-      eval(__codeCache[key]);
+      churchInputExpr = sc_read(new sc_StringInputPort("(" + churchCode + ")"));
+      schemeExpr = compile(churchInputExpr, null);
+      var wrappedSchemeExpr = scheme2jsTemplate.supplant(
+        { churchprogram : String(schemeExpr).slice(1,-1) });
     } catch (err) {
+      compilationSuccess = false;
       returnValueHandler(err);
     }
-  } else {
-    console.log("submitting to scheme2js server");
-    var url="http://focusedattention.org/scheme2js/scheme2js.php?scheme=" +
-      encodeURIComponent(wrappedSchemeExpr) +
-      "&callback=?";
-      jQuery.getJSON(url, null, function(compiledCode){
-              __codeCache[key] = compiledCode;
-              // Set output ports
-              SC_DEFAULT_OUT = new sc_GenericOutputPort(returnValueHandler);
-              SC_ERROR_OUT = SC_DEFAULT_OUT;
-              // Run compiled file
-              try {
-                eval(compiledCode);
-              } catch (err) {
-                returnValueHandler(err);
-              }
-      });
+    if (!(compilationSuccess)) {
+      return false;
     }
+
+    console.log("submitting to scheme2js server");
+    jQuery.getJSON(
+      scheme2jsURL(wrappedSchemeExpr),
+      null,
+      function(compiledCode){
+        __codeCache[key] = compiledCode;
+        setScheme2jsPorts(returnValueHandler);
+        safeDelayedEval(compiledCode, returnValueHandler, 0);
+      });
+  }
 };
